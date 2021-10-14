7e058f756a60296e3212c575c4a6445015aa21b3
Kshitij Chaurasiya <kshitij.chaurasiya@gmail.com>
2021-10-15 02:12:48 +0530

    Remove commented example form PlayGame

--- src/com/snake_ladder_game/PlayGame.java
+++ src/com/snake_ladder_game/PlayGame.java
@@ -6,28 +6,8 @@ public class PlayGame {
 
 	public static void main(String[] args) {
 		/*
-9
-62 5
-33 6
-49 9
-88 16
-41 20
-56 53
-98 64
-93 73
-95 75
-8
-2 37
-27 46
-10 32
-51 68
-61 79
-65 84
-71 91
-81 100
-2
-Gaurav
-Sagar 
+		 * Game Start
+		 * Input for this file there in ReadMe.md
 		 */
 		 GameBoard<Integer, Integer> gameBoard = new GameBoard<Integer, Integer>();
 		 Dice dice = new Dice();
